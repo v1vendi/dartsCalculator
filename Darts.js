13f17722e12a31dc3ae00de2303edb3ad690480a
Ilya Komsa <ilya.komsa@gmail.com>
2015-06-05 01:35:08 +0300

    change shot calculation from per-turn to per-shot
    crashes rating calculation :(

--- Darts.js
+++ Darts.js
@@ -1,67 +1,138 @@
-﻿function Game(players, container) {
-
-    var game = this;
-
-    this.players = players.slice().shuffle();
-    this.currentPlayer = this.players[0];
-    this.score = [];
+﻿function Darts(players, container, scoreToWin, straightOut) {
     
-    this.finished = false;
-
-    players.forEach(function (player, index) {
-        game.score[player.name] = [301];
+    var game = this;
+    
+    scoreToWin = scoreToWin || 301;
+    
+    game.straightOut = straightOut === false ? false : true;
+    
+    game.players = players.map(function (p) {
+        return new Darts.player(game, p, scoreToWin);
     });
-
-    this.container = container;
+    
+    game.currentPlayer = this.players[0];
+    
+    game.turnsCount = 1;
+    game.container = container;
 }
 
-Game.prototype.throw = function (score) {
+Darts.player = function (game, player, score) {
+    
+    this.game = game;
+    this.name = player.name;
+    this.score = score;
+    this.scoreHistory = [score];
+    
+    this.checkedOut = false;
+    this.turn = [];
+    this.turnsCount = 0;
+}
 
+Darts.player.prototype.shoot = function (score, sector) {
+    
+    var player = this;
+    
+    var shotScore = score * sector;
+    
     if (score != parseInt(score)) {
         alert('Вводи только цифры, блеать');
-        this.render();
-        return;
+        return Darts.shotResults.mistake;
     }
-
-    if (score > 180) {
+    
+    if (score > 20 && score != 25) {
         alert('Не пизди, блеать');
-        return;
+        return Darts.shotResults.mistake;
     }
-
-    var currentPlayerScores = this.score[this.currentPlayer.name];
-
-    var lastScore = this.score[this.currentPlayer.name].last();
-
-    if (lastScore < score) {
-        currentPlayerScores.push(lastScore);
-    } else {
-        currentPlayerScores.push(lastScore - score);
-    }
-
-    if (this.currentPlayer === this.players.last()) {
-        this.validateWinner();
-    }
-
-    var finishedPlayers = this.players.filter(function (player) {
-        return !player.finished;
+    
+    var turnScore = shotScore;
+    
+    player.turn.forEach(function (shot) {
+        turnScore += shot;
     });
-
-    if (finishedPlayers.length <= 1) {
-        this.endGame();
-        return;
+    
+    if (player.score < turnScore) {
+        
+        player.scoreHistory.push('-');
+        return Darts.shotResults.bust;
     }
+    
+    if (!player.game.straightOut && player.score - turnScore === 1) {
+        
+        player.scoreHistory.push('-');
+        return Darts.shotResults.bust;
+    }
+    
+    player.turn.push(shotScore);
+    
+    if (player.score === turnScore) {
+        player.score = 0;
+        player.checkedOut = true;
+        player.scoreHistory.push(0);
+        player.turn.length = 0;
+        player.turnsCount++;
+        
+        return Darts.shotResults.checkOut;
+    }
+    
+    if (player.turn.length === 3) {
+        player.score -= turnScore;
+        player.scoreHistory.push(player.score);
+        player.turn.length = 0;
+        player.turnsCount++;
+        return Darts.shotResults.endTurn;
+    }
+    
+    return Darts.shotResults.ok;
+}
 
-    this.setNextPlayer();
+Darts.sector = {
+    single: 1,
+    double: 2, 
+    triple: 3
+}
 
+Darts.shotResults = {
+    mistake: 2,
+    bust: -1,
+    ok: 0,
+    checkOut: 1,
+    endTurn: 3
+}
+
+Darts.prototype.throw = function (score, sector) {
+    
+    sector = sector || Darts.sector.single;
+    
+    var shotResult = this.currentPlayer.shoot(score, sector);
+    
+    var remainingPlayers = this.players.filter(function (player) {
+        return !player.checkedOut;
+    });
+    
+    if (shotResult === Darts.shotResults.checkOut || shotResult === Darts.shotResults.endTurn || shotResult === Darts.shotResults.bust) {
+
+        if (remainingPlayers.length <= 1 && this.currentPlayer === remainingPlayers[remainingPlayers.length -1]) {
+            
+            this.render();
+            this.endGame();
+            return;
+        }        
+        
+        if (this.currentPlayer === remainingPlayers[0]) {
+            this.turnsCount++;
+        }
+
+        this.setNextPlayer();
+    }
+    
     this.render();
 }
 
-Game.prototype.endGame = function () {
+Darts.prototype.endGame = function () {
     var game = this;
-
+    
     if (window.confirm('Закончили?')) {
-        game.finished = true;
-
+        
         var endGameEvent = new CustomEvent('gameEnd', {
             detail: {
                 results: game.players
@@ -71,73 +142,74 @@ Game.prototype.endGame = function () {
     }
 };
 
-Game.prototype.validateWinner = function () {
-    var game = this;
-
-    this.winners = this.players.filter(function (player) {
-        return !player.finished && game.score[player.name].last() === 0;
-    });
-
-    this.winners.forEach(function (player) {
-
-        player.finished = true;
-        player.shots = game.score[player.name].length - 1;
-    });
-
-};
-
-Game.prototype.setNextPlayer = function () {
-
+Darts.prototype.setNextPlayer = function () {
+    
     var currentPlayerIndex = this.players.indexOf(this.currentPlayer);
-
-    if (currentPlayerIndex === this.players.length - 1) {
+    
+    if (this.currentPlayer === this.players[this.players.length - 1) {
         this.currentPlayer = this.players[0];
+
     } else {
         this.currentPlayer = this.players[currentPlayerIndex + 1];
     }
-
-    if (this.currentPlayer.finished) {
+    
+    if (this.currentPlayer.checkedOut) {
         this.setNextPlayer();
     }
 }
 
-Game.prototype.render = function () {
+Darts.prototype.render = function () {
     var game = this;
-
+    
     var tableHeader = this.container.querySelector('thead tr');
     var tbody = this.container.querySelector('tbody');
     tableHeader.innerHTML = '';
     tbody.innerHTML = '';
-
+    
     this.players.forEach(function (player) {
-
+        
         var th = document.createElement('th');
         th.textContent = player.name;
-
+        
         if (player === game.currentPlayer) {
             th.classList.add('active');
         }
-
+        
         tableHeader.appendChild(th);
     });
-
+    
     var firstPlayer = this.players[0];
-    var rounds = this.score && this.score[firstPlayer.name] && this.score[firstPlayer.name].length;
-
-    if (!rounds) {
-        return;
-    }
-
+    var rounds = this.turnsCount;
+    
+    
     for (var i = 0; i < rounds; i++) {
         var tr = document.createElement('tr');
-
+        
         game.players.forEach(function (player) {
             var td = document.createElement('td');
-            td.textContent = game.score[player.name][i];
-
+            td.textContent = player.scoreHistory[i];
+            
             tr.appendChild(td);
         });
-
+        
         tbody.appendChild(tr);
     }
+    
+    var tfoot = this.container.querySelector('tfoot');
+    tfoot.innerHTML = "";
+    
+    game.players.forEach(function (player) {
+        
+        var turnScore = 0;
+        player.turn.forEach(function (t) {
+            turnScore += t;
+        })
+        
+        var currentScore = player.score - turnScore;
+        
+        var th = document.createElement('th');
+        th.textContent = currentScore;
+        
+        tfoot.appendChild(th);
+    });
 }
